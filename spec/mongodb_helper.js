dd2ab4f0c4baa90d45c4ec5fda4b67be4987554c
Anton <antonedeh1@gmail.com>
2026-08-10 15:46:37 +0100

    Pure hopium

--- spec/mongodb_helper.js
+++ spec/mongodb_helper.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 beforeAll(function (done) {
-  mongoose.connect("mongodb://0.0.0.0/acebook_test", {
+  mongoose.connect(process.env.MONGODB_URL || "mongodb://0.0.0.0/acebook_test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
